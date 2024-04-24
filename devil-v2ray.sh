3e800ec137c53df28b6c437be4e1a42dd09a99cd
Morro simon <148977159+Morrow37@users.noreply.github.com>
2024-04-24 01:04:07 +0100

    Update devil-v2ray.sh

--- devil-v2ray.sh
+++ devil-v2ray.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 
-# Config v2ray (remember to replace placeholders)
-
+# Config v2ray (assumes you're using VLESS)
 rm -rf /etc/xray/config.json
 cat << EOF > /etc/xray/config.json
 {
   "inbounds": [
     {
-      "port": $PORT,  # Use port 443 for standard HTTPS traffic
+      "port": $PORT, 
       "protocol": "vless",
       "settings": {
         "decryption": "none",
         "clients": [
           {
-            "id": "$UUID" # Replace with your actual UUID
+            "id": "$UUID", 
+            "flow": "xtls-rprx-direct"  # Enable XTLS for CloudFlare proxying
           }
         ]
       },
       "streamSettings": {
         "network": "ws",
-        "security": "tls",
-        "tlsSettings": {
-          "serverName": "see.sightcall.com" 
+        "security": "xtls",  # Activate XTLS
+        "xtlsSettings": {
+          "serverName": "see.sightcall.com"  # Your bug host
         }
       }
     }
   ],
   "outbounds": [
     {
-      "protocol": "freedom"
+      "protocol": "freedom",
+      "settings": {
+        "domainStrategy": "UseIP"  # May be needed for Heroku
+      }
     }
   ]
 }
